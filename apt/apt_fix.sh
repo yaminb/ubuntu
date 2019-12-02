1c17d3fa84686700d1f9f45e33d8ebf461fea8cd
Yamin <yamin@yamin.ca>
2019-12-02 16:04:51 -0500

    something

--- apt/apt_fix.sh
+++ apt/apt_fix.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -x #echo on
+
+#normally this is due to some package not installing.... try reinstall
+#sudo apt install --reinstall linux-modules-extra-5.3.0-24-generic
+
 sudo apt --fix-broken install
 sudo apt autoremove
 sudo apt autoclean
