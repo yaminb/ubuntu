6024cdf221204a5bd044f4a6a0e190c9f8cf4e35
Yamin <yamin@yamin.ca>
2020-05-20 09:48:47 -0400

    brother printer tings

---
+++ printer/brother_HL-2270DW.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x #echo on
+
+
+
+#wireless report - press GO 5 times in 4 seconds
+
+#run install
+sudo ~/local_apps/brother_printer/linux-brprinter-installer-2.2.1-1
+#Choose to specify the IP address
+
+
