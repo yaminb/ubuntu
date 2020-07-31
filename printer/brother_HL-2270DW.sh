df17383b0d815406c5884dbd9197d3fac9673a5e
Yamin <yamin@yamin.ca>
2020-07-30 21:27:49 -0400

    brother driver

--- printer/brother_HL-2270DW.sh
+++ printer/brother_HL-2270DW.sh
@@ -6,7 +6,7 @@ set -x #echo on
 #wireless report - press GO 5 times in 4 seconds
 
 #run install
-sudo ~/local_apps/brother_printer/linux-brprinter-installer-2.2.1-1
+sudo ~/local_apps/brother_printer/linux-brprinter-installer-2.2.2-1
 #model HL-2270DW
 #Choose to specify the IP address
 
