df99c6128c3588dec68e62b2382a5991239e5fe6
Yamin <yamin@yamin.ca>
2021-10-11 12:20:07 -0400

    wayland hints

---
+++ system/ubuntu_wayland_config_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#This is not a proper script. Just hints on files to configure for wayland
+set -x #echo on
+
+#commend out GOTO="gdm_prefer_xorg"
+sudo vi /lib/udev/rules.d/61-gdm.rules
+
+#GRUB_CMDLINE_LINUX="nvidia-drm.modeset=1"
+sudo vi /etc/default/grub
+
