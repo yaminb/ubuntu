210a546a6711b9ea7f6a8b0223812c82e68a202e
Yamin <yamin@yamin.ca>
2021-11-14 18:20:17 -0500

    pirewire changes

--- audio/audio_restart.sh
+++ audio/audio_restart.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -x #echo on
 
-systemctl --user restart pulseaudio
+#systemctl --user restart pulseaudio
+
+systemctl --user restart pipewire pipewire-pulse
+systemctl --user daemon-reload
+
