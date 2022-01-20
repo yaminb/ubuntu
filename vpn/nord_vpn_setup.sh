a1767414d22fbf25045b024f61c31f0c7597785d
Yamin <yamin@yamin.ca>
2022-01-20 10:44:50 -0500

    vpn stuff

---
+++ vpn/nord_vpn_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x #echo on
+
+#just a note on location to get files
+#https://downloads.nordcdn.com/configs/archives/certificates/servers.zip
+#https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
+
