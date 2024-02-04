af85908383cdede36c197fd3f89392a996854b29
Yamin <yamin@yamin.ca>
2024-02-03 19:24:09 -0500

    nvidia mode set

---
+++ graphics/nvidia_toggle_modeset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#mode settings seems to cause some problems
+set -x #echo on
+
+#modeset should be set here
+#change it to 0 to turn it off. 
+sudo vi /etc/modprobe.d/nvidia-graphics-drivers-kms.conf
+
+
+
+
